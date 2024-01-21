cf7f58f5e95d6425897c3544dddf7ed6eb89cd7a
BHEEMRAV-MAHOVIYA <bheemravma.ora@gmail.com>
2024-01-21 05:59:22 +0530

    First comment in java demo program

---
+++ DemoJavaPrograms/src/com/demo/programs/WelcomeProgram.java
@@ -0,0 +1,7 @@
+package com.demo.programs;
+
+public class WelcomeProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to java in git projects.");
+	}
+}
--- DemoProgramJava/src/com/learn/demo/DemoTest.java
+++
@@ -1,8 +0,0 @@
-package com.learn.demo;
-
-public class DemoTest {
-   public static void main(String[] args) {
-	System.out.print("welcome to java and github");
-	
-}
-}
